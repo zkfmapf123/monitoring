93e0c05824166e5f3a211145d3538fa04729a10c
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2023-08-05 12:51:41 +0900

    add iam

---
+++ infra/iam.tf
@@ -0,0 +1,57 @@
+########################################################################
+### aws iam role
+########################################################################
+resource "aws_iam_role" "ec2_cloudwatch_role" {
+  name = "ec2-cloudwatch-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+########################################################################
+### iam policy
+########################################################################
+resource "aws_iam_policy" "ec2_cloudwatch_policy" {
+  name = "ec2-cloudwatch-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "cloudwatch:PutMetricData"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = "logs:CreateLogGroup"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = "logs:CreateLogStream"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = "logs:PutLogEvents"
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_attchment" {
+  policy_arn = aws_iam_policy.ec2_cloudwatch_policy.arn
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+}
